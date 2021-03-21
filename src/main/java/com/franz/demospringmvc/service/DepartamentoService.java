2cd9bdccb5eaa694f4afcbd014ec0f1305279cf4
franz <fran.sf@uol.com.br>
2021-03-21 16:04:52 -0300

    5.26 - Finalizando os Services

--- src/main/java/com/franz/demospringmvc/service/DepartamentoService.java
+++ src/main/java/com/franz/demospringmvc/service/DepartamentoService.java
@@ -1,5 +1,20 @@
 package com.franz.demospringmvc.service;
 
+import java.util.List;
+
+import com.franz.demospringmvc.domain.Departamento;
+
 public interface DepartamentoService {
+	
+void salvar(Departamento departamento);
+	
+	void editar(Departamento departamento);
+	
+	void excluir(Long id);
+	
+	Departamento buscarPorId(Long id);
+	
+	List<Departamento> buscarTodos();
+	
 
 }
--- src/main/java/com/franz/demospringmvc/service/FuncionarioService.java
+++ src/main/java/com/franz/demospringmvc/service/FuncionarioService.java
@@ -1,5 +1,20 @@
 package com.franz.demospringmvc.service;
 
+import java.util.List;
+
+import com.franz.demospringmvc.domain.Funcionario;
+
 public interface FuncionarioService {
+	
+void salvar(Funcionario funcionario);
+	
+	void editar(Funcionario funcionario);
+	
+	void excluir(Long id);
+	
+	Funcionario buscarPorId(Long id);
+	
+	List<Funcionario> buscarTodos();
+	
 
 }
